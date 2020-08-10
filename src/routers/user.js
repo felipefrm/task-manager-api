147826faf9f67f57ee9d5b882863bf0f4b7f0626
Felipe Melo <narulee@hotmail.com>
2020-08-10 20:24:28 -0300

    user/task relationship

--- src/routers/user.js
+++ src/routers/user.js
@@ -20,7 +20,7 @@ router.post('/users/login', async (req, res) => {
     try { 
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({user, token})
+        res.send({user: user, token})
     } catch(e) {
         res.status(400).send(e)
     }
@@ -50,18 +50,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) 
-            return res.status(404).send()
-        res.send(user)
-    } catch(e) {
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -69,23 +58,18 @@ router.patch('/users/:id', async (req, res) => {
         return res.status(400).send({error: 'Invalid updates!'})
     }
     try {
-        const user = await User.findByIdAndUpdate(req.params.id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-        if (!user)
-            return res.status(404).send()
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
         res.send(user)
     } catch(e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user)
-            return res.status(404).send()
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch(e) {
         res.status(500).send(e)
     }
